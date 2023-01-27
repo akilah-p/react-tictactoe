3d60570b56996b913bd20c457e9d753f90666229
Akilah Powell <atpjun@gmail.com>
2023-01-27 12:06:37 -0800

    set reset and set player

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -7,6 +7,34 @@ const GameProvider = ({ children }) => {
   const [board, setBoard] = useState(gameData);
   const [player, setPlayer] = useState('X');
   const [message, setMessage] = useState('Time to play');
-  const [active, setActive] = useState(true);
+  const [active, setActive] = useState(false);
+
+
+
+  
+  const handleClick = (box) => {
+    const currentBox = board.findIndex((square) => square.space === box.space);
+    const newGame = board;
+    if (!newGame[currentBox].content) {
+      newGame[currentBox] = { space: newGame[currentBox].space, content: player,
+
+      };
+      setBoard(newGame);
+    }
+    setPlayer(player === 'X' ? 'O' : 'X' );
+    setMessage(`Your turn ${player === 'X' ? 'O' : 'X'}`);
+  };
+
+
+
+
+
+
+
+
+  return <GameContext.Provider>{children}</GameContext.Provider>;
+    
+
+
 };
 
---
+++ src/components/Board/Board.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { GameContext } from '../../Context/GameContext';
+import { gameData, winData } from '../../../game-data';
+
+// export default function Board({boxes, player}) {
+ 
+//   return (
+//     <div className="board">Board
+//     {boxes.map((box) =>)}
+//     </div>
+//   )
+// }
