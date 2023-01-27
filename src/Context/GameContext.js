545fddab5aa77b571c4161f27840e5a0ce4bbbda
Akilah Powell <atpjun@gmail.com>
2023-01-27 02:03:57 -0800

    finished setting state with game context

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -3,8 +3,18 @@ import BoardData from '../game-data.js';
 
 const = GameContext = createContext();
 
-const GameProvider = ({ children } => {
+const GameProvider = ({ children }) => {
     const [board, setBoard] = useState(BoardData);
     const [currentPlayer, setCurrentPlayer] = useState('X');
-    const [gameMessage, setGameMessage]
-})
+    const [gameMessage, setGameMessage] = useState(null);
+    const [active, setActive] = useState=(true);
+
+    return(
+        <GameContext.Provider vale={{ board, setBoard, currentPlayer, setCurrentPlayer, gameMessage, setGameMessage, active, setActive }} >
+            {children}
+        </GameContext.Provider>
+
+    );
+};
+
+export { GameContext, GameProvider };
