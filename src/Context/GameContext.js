05449116b36162943212c754f25230c53845185a
Akilah Powell <atpjun@gmail.com>
2023-01-27 13:32:50 -0800

    created new branch to clean up code

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState } from 'react';
 import { gameData, winData } from './game-data.js';
 
 const GameContext = createContext();
@@ -8,30 +8,53 @@ const GameProvider = ({ children }) => {
   const [player, setPlayer] = useState('X');
   const [message, setMessage] = useState('Time to play');
   const [active, setActive] = useState(false);
+  const [winner, setWinner] = useState(false);
+  const [turns, setTurns] = useState(9);
 
-
-
-  
-  const handleClick = (box) => {
-    const currentBox = board.findIndex((square) => square.space === box.space);
-    const newGame = board;
-    if (!newGame[currentBox].content) {
-      newGame[currentBox] = { space: newGame[currentBox].space, content: player,
-
-      };
-      setBoard(newGame);
-    }
-    setPlayer(player === 'X' ? 'O' : 'X');
-    setMessage(`Your turn ${player === 'X' ? 'O' : 'X'}`);
+  const setTurn = () => {
+    player === 'X' ? setPlayer('O') : setPlayer('X');
   };
 
+  function checkWin(win) {
+    const rows = (a, b, c) => {
+      if (win[a].content === '' || win[b].content === '' || win[c].content === '')
+        return false;
+      return (
+        win[a].content === win[b].content && win[b].content === win[c].content);
+  
+    };
 
+    return rows(winData);
+  }
 
+  const resetGame = () => {
+    setBoard(gameData);
+    setPlayer('X');
+    setWinner(false);
+    setTurns(9);
+  };
 
+  const handleClick = (box) => {
+    if (box.content !== '') return;
+    const newBoard = board.map((squares) => {
+      if (squares.space === box.space) {
+        return {
+          space: box.space,
+          content: (player === 'X' ? 'X' : 'O') };
+      }
+      return squares;
+    
+    });
 
+    setBoard(newBoard);
+    setTurns(prev => prev - 1);
 
-
-
+    if (checkWin(newBoard)) {
+      setWinner(player);
+    }
+    setTurn();
+  };
+    
   return <GameContext.Provider
     value={{
       board,
@@ -43,6 +66,9 @@ const GameProvider = ({ children }) => {
       message,
       setMessage,
       handleClick,
+      resetGame, 
+      winner,
+      turns
   
     }}
   >{children}</GameContext.Provider>;
