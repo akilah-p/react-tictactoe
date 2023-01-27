94f7f7a5d7972395d5a2f6a93cbb7c1565c2bddc
Akilah Powell <atpjun@gmail.com>
2023-01-27 12:42:57 -0800

    added mapping in board.js

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../../Context/GameContext';
+import Box from '../Box';
+import './Board.css';
 
 
 export default function Board() {
-  const { board, handleClick, active } = useContext(GameContext);
-
- 
+  const { board } = useContext(GameContext);
   return (
-    <div className ='board'>
-      {board.map}
+    <div className="board">
+      {board.map((box) => (
+        <Box key={box.space} content={box.content} space={box.space} />
+      ))}
     </div>
-  )
-  
+  );
 }
