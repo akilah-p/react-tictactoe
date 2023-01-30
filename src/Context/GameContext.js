0faca349d01702ca2ec67b1109716e7f88fd050e
Akilah Powell <atpjun@gmail.com>
2023-01-30 08:16:31 -0800

    updating naming conventions, i made it confusing

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -8,7 +8,7 @@ const GameProvider = ({ children }) => {
   const [player, setPlayer] = useState('X');
   const [message, setMessage] = useState('Time to play');
   const [winner, setWinner] = useState('');
-  const [endGame, setEndGame] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
   const [tie, setTie] = useState(false);
 
   useEffect(() => {
@@ -16,11 +16,11 @@ const GameProvider = ({ children }) => {
       if (tie) {
         setMessage('It is a tie!');
       } else {
-        winner && endGame ? setMessage(`${winner} wins this time!`) : setMessage(`${player} it's your turn`);
+        winner && gameOver ? setMessage(`${winner} wins this time!`) : setMessage(`${player} it's your turn`);
       }
     };
     checkWinner();
-  }, [player, winner, endGame, tie]);
+  }, [player, winner, gameOver, tie]);
 
 
   function resetGameButton() {
@@ -29,15 +29,15 @@ const GameProvider = ({ children }) => {
     setWinner();
     setTie(false);
     setMessage(`${player} it's your turn`);
-    setEndGame(false);
+    setGameOver(false);
   }
 
   function handleClick(content, space) {
     if (content !== '') return;
-    if (endGame === true) return;
+    if (gameOver === true) return;
 
     board[space].content = player;
-    handleEndGame();
+    handleGameOver();
     if (player === 'O') {
       setPlayer('X');
     } else {
@@ -46,13 +46,13 @@ const GameProvider = ({ children }) => {
   }
 
     //setting game logic 
-  function handleEndGame() {
+  function handleGameOver() {
     if (
       board[0].content === player && 
         board[1].content === player &&
         board[2].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     }
     if (
@@ -60,7 +60,7 @@ const GameProvider = ({ children }) => {
         board[3].content === player &&
         board[6].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player); 
     }
     if (
@@ -68,21 +68,21 @@ const GameProvider = ({ children }) => {
       board[4].content === player &&
       board[8].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     } if (
       board[1].content === player && 
     board[4].content === player &&
     board[7].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     } if (
       board[2].content === player && 
     board[5].content === player &&
     board[8].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     }
     if (
@@ -90,7 +90,7 @@ const GameProvider = ({ children }) => {
     board[4].content === player &&
     board[5].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     }
     if (
@@ -98,7 +98,7 @@ const GameProvider = ({ children }) => {
     board[7].content === player &&
     board[8].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     }
     if (
@@ -106,7 +106,7 @@ const GameProvider = ({ children }) => {
   board[4].content === player &&
   board[2].content === player
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setWinner(player);
     }
     handleTie();
@@ -125,7 +125,7 @@ const GameProvider = ({ children }) => {
       board[7].content !== '' &&
       board[8].content !== '' 
     ) {
-      setEndGame(true);
+      setGameOver(true);
       setTie(true);
     }
 
